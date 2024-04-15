18f2409f66128ef5bd26857eb3c910ce6c3edf11
Berenguer Pou <berenguer.pou@gmail.com>
2024-04-15 17:51:55 +0200

    test 3: combination length

--- src/checkCombination.test.ts
+++ src/checkCombination.test.ts
@@ -5,10 +5,18 @@ describe("given a checkCombination function", () => {
   test("function must be defined", () => {
     expect(checkCombination).toBeDefined();
   });
+
   test("when a string is passed as argument returns a boolean", () => {
     expect(typeof checkCombination("((((")).toBe("boolean");
   });
+
   test("when a string received contains any character not allowed returns false", () => {
     expect(checkCombination("((a((")).toBe(false);
+    expect(checkCombination("(([[[[}}}}}]]]]))")).toBe(true);
+    expect(checkCombination("")).toBe(true);
+  });
+
+  test("when a string received has odd length ", () => {
+    expect(checkCombination("([(")).toBe(false);
   });
 });
