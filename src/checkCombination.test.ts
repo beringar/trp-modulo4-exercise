7210691494fabd1ce72dcb5a428d3478390f0579
Berenguer Pou <berenguer.pou@gmail.com>
2024-04-15 17:27:46 +0200

    write test to check string argument passed

--- src/checkCombination.test.ts
+++ src/checkCombination.test.ts
@@ -5,4 +5,9 @@ describe("given a checkCombination function", () => {
   test("function must be defined", () => {
     expect(checkCombination).toBeDefined();
   });
+  test("must accept a string as argument", () => {
+    expect(checkCombination("")).not.toThrow();
+
+    expect(checkCombination(1)).toThrow();
+  });
 });
